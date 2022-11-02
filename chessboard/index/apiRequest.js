a427e92b13f15f9e26642dbe288eb7da424faf2e
Logesh-N-R <84169332+Logesh-N-R@users.noreply.github.com>
2022-11-02 15:34:08 +0530

    Update apiRequest.js

--- chessboard/index/apiRequest.js
+++ chessboard/index/apiRequest.js
@@ -1,31 +1,32 @@
 
 // api request to the server
 async function apiRequest(requestFor, apiInput = "") {
+    var hosting = "https://chess-pu59.onrender.com:10000/"
     var apiHit = {
     }
     switch (requestFor) {
         case 'setTheme':
-            apiHit.url = "http://localhost:3000/460cdea15f7e2d7cf366d75da91d8e24475ce5c8921303f808b2eb68064f2f4a";
+            apiHit.url = `${hosting}460cdea15f7e2d7cf366d75da91d8e24475ce5c8921303f808b2eb68064f2f4a`;
             apiHit.data = JSON.stringify(apiInput);
             apiHit.method = 'POST';
             break;
         case 'setcode':
-            apiHit.url = "http://localhost:3000/c6a1245e144765e5894c139b433020e5b200b3185a493978d05c2c74a7116b7b";
+            apiHit.url = `${hosting}c6a1245e144765e5894c139b433020e5b200b3185a493978d05c2c74a7116b7b`;
             apiHit.data = JSON.stringify(apiInput);
             apiHit.method = 'POST';
             break;
         case 'fetchTheme':
-            apiHit.url = "http://localhost:3000/209d605ee355854c2765e7caa274af6c23c29b9f3226145fb8ca8628bbde24b5";
+            apiHit.url = `${hosting}209d605ee355854c2765e7caa274af6c23c29b9f3226145fb8ca8628bbde24b5`;
             apiHit.data = JSON.stringify(apiInput);
             apiHit.method = 'POST';
             break;
         case 'getApprove':
-            apiHit.url = "http://localhost:3000/f931f09cc70252db573bd399ca768cb913173b3c9112003085f757200673eb3a";
+            apiHit.url = `${hosting}f931f09cc70252db573bd399ca768cb913173b3c9112003085f757200673eb3a`;
             apiHit.data = JSON.stringify(apiInput);
             apiHit.method = 'POST';
             break;
         case 'move':
-            apiHit.url = "http://localhost:3000/683a62ce15fbabb1ac867022e56e6e4f4f581762d77c3abb2f8dc8b165b3b1b9";
+            apiHit.url = `${hosting}683a62ce15fbabb1ac867022e56e6e4f4f581762d77c3abb2f8dc8b165b3b1b9`;
             apiHit.data = JSON.stringify(apiInput);
             apiHit.method = 'POST';
             break;
